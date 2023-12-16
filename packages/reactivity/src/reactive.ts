af3d20faa116845d638168ae04b4d2e470ff0f8e
纪宇航 <jiyuhang@jctrans.net>
2023-12-16 12:42:02 +0800

    feat: init isReadonly

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -17,6 +17,9 @@ export interface Target {
   [ReactiveFlags.RAW]?: any
 }
 export const reactiveMap = new WeakMap<Target, any>()
+export const shallowReactiveMap = new WeakMap<Target, any>()
+export const readonlyMap = new WeakMap<Target, any>()
+export const shallowReadonlyMap = new WeakMap<Target, any>()
 
 export function reactive<T extends object>(target: T): T {
   return createReactiveObject(target, false, reactiveMap)
@@ -40,12 +43,32 @@ function createReactiveObject(
   const proxy = new Proxy(target, {
     get(target: Target, key: string | symbol, receiver: object) {
       let _isReadonly = false
+      let shallow = false
       const res = Reflect.get(target, key, receiver)
       console.log('get...', key)
       if (key === ReactiveFlags.IS_REACTIVE) {
         return !_isReadonly
       } else if (key === ReactiveFlags.IS_READONLY) {
         return _isReadonly
+      }else if (key === ReactiveFlags.RAW) {
+        if (
+          receiver ===
+            (isReadonly
+              ? shallow
+                ? shallowReadonlyMap
+                : readonlyMap
+              : shallow
+                ? shallowReactiveMap
+                : reactiveMap
+            ).get(target) ||
+          // receiver is not the reactive proxy, but has the same prototype
+          // this means the reciever is a user proxy of the reactive proxy
+          Object.getPrototypeOf(target) === Object.getPrototypeOf(receiver)
+        ) {
+          return target
+        }
+        // early return undefined
+        return
       }
       track(target, key)
       return res
@@ -62,6 +85,13 @@ function createReactiveObject(
   return proxy
 }
 
+export function isReadonly(value: unknown): boolean {
+  return !!(value && (value as Target)[ReactiveFlags.IS_READONLY])
+}
+
 export function isReactive(value: unknown): boolean {
+  if (isReadonly(value)) {
+    return isReactive((value as Target)[ReactiveFlags.RAW])
+  }
   return !!(value && (value as Target)[ReactiveFlags.IS_REACTIVE])
 }
