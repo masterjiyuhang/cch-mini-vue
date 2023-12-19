e789747f8228800ab275f5377b3c562d38bbfc23
纪宇航 <jiyuhang@jctrans.net>
2023-12-19 21:13:43 +0800

    feat: update shared, init hasChanged

--- packages/shared/src/general.ts
+++ packages/shared/src/general.ts
@@ -15,3 +15,7 @@ export const hasOwn = (
 
 export const isObject = (val: unknown): val is Record<any, any> =>
   val !== null && typeof val === 'object'
+
+// compare whether a value has changed, accounting for NaN.
+export const hasChanged = (value: any, oldValue: any): boolean =>
+  !Object.is(value, oldValue)
