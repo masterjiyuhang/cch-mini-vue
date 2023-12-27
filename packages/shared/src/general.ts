b3a5a040a31d8ada97d8c4125013994b1fe40b7a
纪宇航 <jiyuhang@jctrans.net>
2023-12-27 10:17:33 +0800

    feat: update general shared

--- packages/shared/src/general.ts
+++ packages/shared/src/general.ts
@@ -1,5 +1,26 @@
 export const isArray = Array.isArray
 
+export const isObject = (val: unknown): val is Record<any, any> =>
+  val !== null && typeof val === 'object'
+
+export const isString = (val: unknown): val is string => typeof val === 'string'
+export const isSymbol = (val: unknown): val is symbol => typeof val === 'symbol'
+export const isFunction = (val: unknown): val is Function =>
+  typeof val === 'function'
+
+export const objectToString = Object.prototype.toString
+export const toTypeString = (value: unknown): string =>
+  objectToString.call(value)
+
+export const isDate = (val: unknown): val is Date =>
+  toTypeString(val) === '[object Date]'
+export const isRegExp = (val: unknown): val is RegExp =>
+  toTypeString(val) === '[object RegExp]'
+export const isMap = (val: unknown): val is Map<any, any> =>
+  toTypeString(val) === '[object Map]'
+export const isSet = (val: unknown): val is Set<any> =>
+  toTypeString(val) === '[object Set]'
+
 export const remove = <T>(arr: T[], el: T) => {
   const i = arr.indexOf(el)
   if (i > -1) {
@@ -12,9 +33,6 @@ export const hasOwn = (
   val: object,
   key: string | symbol
 ): key is keyof typeof val => hasOwnProperty.call(val, key)
-
-export const isObject = (val: unknown): val is Record<any, any> =>
-  val !== null && typeof val === 'object'
 
 // compare whether a value has changed, accounting for NaN.
 export const hasChanged = (value: any, oldValue: any): boolean =>
